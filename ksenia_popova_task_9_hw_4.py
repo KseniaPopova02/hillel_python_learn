2d58d2049fa4483c90f98efdd4686a03e1572505
KseniaPopova02 <98548104+KseniaPopova02@users.noreply.github.com>
2022-02-23 23:04:09 +0200

    Add files via upload

---
+++ ksenia_popova_task_9_hw_4.py
@@ -0,0 +1,15 @@
+a = [8, 7, 10]
+b = [4, 7, 9]
+for i in a:
+    for k in b:
+        if i in b:
+            if k in a:
+                print(True)
+                break
+              
+
+
+
+
+
+
